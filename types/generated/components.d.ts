dcfdf571bbe8eeda4febb46c6abc979a2998a410
bipolab-eu <bentagai@bipolab.eu>
2025-06-12 16:33:03 +0100

    create hero component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,23 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksHero extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_heroes';
+  info: {
+    displayName: 'hero';
+  };
+  attributes: {
+    cover: Schema.Attribute.Media<'images'>;
+    linkTitle: Schema.Attribute.String;
+    linkUrl: Schema.Attribute.String;
+    subtitle: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.hero': BlocksHero;
+    }
+  }
+}
